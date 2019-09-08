95e139e1511cf92d5452640303e4f9c8b45ea4d3
“lihongta” <1021252402@qq.com>
2019-09-08 22:23:50 +0800

    add

---
+++ 99_table.py
@@ -0,0 +1,13 @@
+# 输出99乘法表
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        # n = i*j
+        if i == 3 or i == 4:
+            if j == 3:
+                print(" ",end="")
+        print("%d*%d=%d " % (j,i,(i*j)),end="")
+        j += 1
+    i += 1
+    print()
