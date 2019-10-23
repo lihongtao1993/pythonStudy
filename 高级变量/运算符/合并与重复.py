de595a35f78be17772b202eb4a33ddfd761764b3
“lihongta” <1021252402@qq.com>
2019-10-23 22:54:19 +0800

    运算符

---
+++ 高级变量/运算符/合并与重复.py
@@ -0,0 +1,21 @@
+# 在python中，可以使用+来合并字符串、列表、元组
+
+# 字符串合并
+str1 = "hello"+"world"
+print(str1)
+
+# 列表合并
+List1 = [1, 2, 3]
+List2 = [11, 22, 33]
+List3 = List1 + List2
+print(List3)
+
+# 元组合并
+
+tuple1 = (1, 2, 3)
+tuple2 = (11, 12, 13)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# 注意字典不能使用合并操作
+
---
+++ 高级变量/运算符/成员运算符.py
@@ -0,0 +1,15 @@
+# 成员运算符：in 和 not in ，
+# 支持的数据类型：字符串、列表、元组、字典
+
+# in运算符
+print("字符串：")
+print("a" in "abcd")
+print("cf" in "abcd")
+print("列表：")
+print(3 in [1, 2, 3, 4])
+print(33 in [1, 2, 3, 4])
+print("字典：")
+print("a" in {"a": "b"})
+print("b" in {"a": "b"})
+# 字典值判断key值，不判断value
+
