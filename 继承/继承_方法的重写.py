3b8d2a6211c93ad5f802ca99488740e11d4dc293
htli <1021252402@qq.com>
2020-07-28 23:22:51 +0800

    add方法的重写

--- 继承/继承_方法的重写.py
+++ 继承/继承_方法的重写.py
@@ -1,3 +1,5 @@
+# 当父类的方法不能满足子类的需求时，比如柯基狗的叫声不是汪汪汪，而是哈哈哈
+# 实现的方法，在子类中定义一个同名的方法
 class Animal:
 
     def eat(self):
@@ -22,9 +24,11 @@ class Keji(Dog):
     def shout(self):
         print("我是柯基犬")
 
+    def bark(self):
+        print("哈哈哈……，柯基叫的不一样")
+
 
 Kj = Keji()
 
 Kj.shout()
-Kj.run()
-Kj.eat()
+Kj.bark()
